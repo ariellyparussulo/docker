a99a0d41c09b66ef390b733b994c797871be9cd5
Arielly <ariellyblabla@gmail.com>
2019-05-01 14:37:16 -0300

    Create containers, create README.md and docker-compose file.

---
+++ number_of_visits_website/README.md
@@ -0,0 +1,11 @@
+# Main goal
+
+This application counts the number of users that accessed this application using a nodeJS application and a redis server.
+
+## Instructions
+
+To run the redis server, use the follow command:
+
+```sh
+
+```
---
+++ number_of_visits_website/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const redis = require('redis')
+
+const app = express()
+
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+})
+
+const REDIS_VISITS = 'visits'
+
+
+function getVisits(res) {
+    client.get(REDIS_VISITS, (err, visits) => {
+        if (err) {
+            res.send('Oops...something happened. ')
+            return
+        }
+
+        res.send(`Number of visits is ${visits}`)
+        updateeNumberOfVisits(visits)
+    })
+}
+
+function updateeNumberOfVisits(visits) {
+    client.setget(REDIS_VISITS, parseInt(visits) + 1)
+}
+
+app.get('/', (req, res) => {
+    getVisits(res)
+})
+
+app.listen(8081, () => {
+    console.log('Running application in 8081 port...')    
+})
